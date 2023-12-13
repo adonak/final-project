e758144313a227f987e2681c6f158ffbe768b8a3
adonak <andy.donakowski@gmail.com>
2023-12-12 19:02:04 -0600

    Added variable drop-down menu

--- js/main.js
+++ js/main.js
@@ -1,25 +1,97 @@
-// create leaflet map
-var map = L.map('map').setView([40, -90], 6.4);  // centered on IL
+// Declare dropdown globally
+var dropdown;
 
+// create leaflet map
+var map = L.map('map').setView([40, -90], 6.4);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var geojsonLayer; // declare geojsonLayer outside the Promise.all block
-var selectedFeatures = new Set(); // Set to store selected features
+var geojsonLayer;
+var selectedFeatures = new Set();
+var chartData = {
+    labels: ['2010', '2015', '2020'],
+    datasets: [{
+        label: 'Change Over Time',
+        backgroundColor: ['rgba(255, 99, 132, 0.7)', 'rgba(54, 162, 235, 0.7)', 'rgba(255, 206, 86, 0.7)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+        borderWidth: 1,
+        data: [0, 0, 0]
+    }]
+};
 
-// load topojson and csv data
+var barChartCanvas = document.getElementById('barChart').getContext('2d');
+var barChart = new Chart(barChartCanvas, {
+    type: 'bar',
+    data: chartData,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+// Function to populate the dropdown menu with variables from the CSV
+function populateDropdown(csvData) {
+    dropdown = document.getElementById('variableDropdown');
+    var uniqueVariables = new Set();
+
+    // Assuming that the CSV variable names follow a pattern like "{variable}_{year}"
+    csvData.forEach(function (csvRecord) {
+        Object.keys(csvRecord).forEach(function (key) {
+            var parts = key.split('_');
+            if (parts.length === 2 && !uniqueVariables.has(parts[0])) {
+                uniqueVariables.add(parts[0]);
+            }
+        });
+    });
+
+    console.log('Unique Variables:', Array.from(uniqueVariables)); // Debug statement
+
+    // Clear existing options
+    dropdown.innerHTML = '';
+
+    // Add a default option
+    var defaultOption = document.createElement('option');
+    defaultOption.text = 'Select Variable';
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    dropdown.add(defaultOption);
+
+    // Add options based on unique variables
+    uniqueVariables.forEach(function (variable) {
+        var option = document.createElement('option');
+        option.value = variable;
+        option.text = variable;
+        dropdown.add(option);
+    });
+
+    // Dropdown change event to update the chart
+    dropdown.addEventListener('change', updateChartVariable);
+}
+
+// Function to update the chart based on the selected variable
+function updateChartVariable() {
+    // Use 'pop' as the default variable
+    var selectedVariable = dropdown.value || 'pop';
+    
+    // Call the chart update function here
+    updateChartData(selectedVariable);
+    console.log('Selected Variable:', selectedVariable);
+}
+
+
 Promise.all([
     d3.json('data/il_tracts_2020_wgs84.topojson'),
     d3.csv('data/marketCharacteristics_table.csv')
-]).then(function([topojsonData, csvData]) {
-    // convert topojson to geojson
+]).then(function ([topojsonData, csvData]) {
     var geojsonData = topojson.feature(topojsonData, topojsonData.objects.il_tracts_2020_wgs84);
 
-    // join csv data with geojson based on id fields
-    geojsonData.features.forEach(function(feature) {
+    geojsonData.features.forEach(function (feature) {
         var uniqueID = feature.properties.GEOID;
-        var csvRecord = csvData.find(function(csvRecord) {
+        var csvRecord = csvData.find(function (csvRecord) {
             return csvRecord.geog === uniqueID;
         });
         if (csvRecord) {
@@ -29,78 +101,62 @@ Promise.all([
         }
     });
 
-    // create geojson layer with combined data and add it to map
     geojsonLayer = L.geoJson(geojsonData, {
         style: function (feature) {
             return { fillColor: 'green', weight: 0.25, opacity: 1, color: 'white', fillOpacity: 0.7 };
         },
         onEachFeature: function (feature, layer) {
-            // Add click event listener
             layer.on('click', function () {
-                // Toggle the selected state of the feature
                 if (selectedFeatures.has(feature)) {
                     selectedFeatures.delete(feature);
                 } else {
                     selectedFeatures.add(feature);
                 }
-
-                // Update the highlight
                 updateHighlight();
+                updateChartData();
             });
 
-            // add hover event listener
             layer.on('mouseover', function (event) {
-                showLabel(event.latlng, '<p><b> Census tract: ' + '</b>' +feature.properties.GEOID + '</p>');
+                showLabel(event.latlng, '<p><b> Census tract: ' + '</b>' + feature.properties.GEOID + '</p>');
             });
 
-            // remove label on mouseout
             layer.on('mouseout', function () {
                 hideLabel();
             });
         }
     }).addTo(map);
+
+    // Populate the dropdown menu
+    populateDropdown(csvData);
 });
 
-// Function to update the highlight
 function updateHighlight() {
     geojsonLayer.eachLayer(function (layer) {
         if (selectedFeatures.has(layer.feature)) {
-            // Highlight selected features
             layer.setStyle({ fillColor: 'blue', color: 'black' });
         } else {
-            // Reset the style for non-selected features
             layer.setStyle({ fillColor: 'green', weight: 0.25, opacity: 1, color: 'white', fillOpacity: 0.7 });
         }
     });
-
-    // Update the selected features list
-    updateSelectedList();
 }
 
+function updateChartData() {
+    var selectedPop_2010 = 0;
+    var selectedPop_2015 = 0;
+    var selectedPop_2020 = 0;
 
-var tooltip; // Declare tooltip variable
-var lastClickedLayer; // Variable to store the last clicked layer
-
-// Function to highlight a feature
-function highlightFeature(layer) {
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
+    selectedFeatures.forEach(function (feature) {
+        selectedPop_2010 += feature.properties.pop_2010 || 0;
+        selectedPop_2015 += feature.properties.pop_2015 || 0;
+        selectedPop_2020 += feature.properties.pop_2020 || 0;
     });
+
+    chartData.datasets[0].data = [selectedPop_2010, selectedPop_2015, selectedPop_2020];
+    barChart.update();
 }
 
-// Function to reset the highlight on a layer
-function resetHighlight(layer) {
-    geojsonLayer.resetStyle(layer);
-}
-
-// Function to show label
 function showLabel(latlng, label) {
     if (!label) return;
-
-    // Create a custom tooltip
     tooltip = L.tooltip({
         permanent: true,
         direction: 'top',
@@ -111,17 +167,13 @@ function showLabel(latlng, label) {
         .addTo(map);
 }
 
-// Function to hide label
 function hideLabel() {
-    // Check if tooltip is defined before attempting to close it
     if (tooltip) {
         tooltip.removeFrom(map);
-        tooltip = undefined; // Set it to undefined after removal
+        tooltip = undefined;
     }
 }
 
-
-// Create a button to clear selections
 var clearSelectionButton = L.control({ position: 'bottomleft' });
 
 clearSelectionButton.onAdd = function (map) {
@@ -132,28 +184,8 @@ clearSelectionButton.onAdd = function (map) {
 
 clearSelectionButton.addTo(map);
 
-// Function to clear selections
 function clearSelection() {
     selectedFeatures.clear();
     updateHighlight();
+    updateChartData();
 }
-
-// Function to update the list of selected features
-function updateSelectedList() {
-    const selectedList = document.getElementById('selected-list');
-    selectedList.innerHTML = ''; // Clear the existing list
-
-    selectedFeatures.forEach(feature => {
-        const listItem = document.createElement('li');
-        listItem.textContent = feature.properties.GEOID;
-        selectedList.appendChild(listItem);
-    });
-}
-
-// Function to clear selections
-function clearSelection() {
-    selectedFeatures.clear();
-    updateHighlight();
-    updateSelectedList(); // Update the list when selections are cleared
-}
-
